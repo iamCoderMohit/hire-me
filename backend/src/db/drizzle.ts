9c1fd6c90e07fdca390008c71b276455a4991629
iamCoderMohitt <mohitjoshiu@gmail.com>
2026-02-18 19:05:40 +0530

    fix

--- backend/src/db/drizzle.ts
+++ backend/src/db/drizzle.ts
@@ -2,7 +2,6 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-//   connectionString: "postgresql://postgres.csurksgxzbhhoyzwdsvn:mXMQMpaEUCy1HJ3b@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres",
 connectionString: process.env.SUPABASE_DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
